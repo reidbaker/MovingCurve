8222ad5a3cb4ade821c3e4ddec001c427497288b
Makario Lewis <makariolewis@gmail.com>
2012-09-08 15:33:02 -0400

    Added more curves and points

--- src/MovingCurve.java
+++ src/MovingCurve.java
@@ -11,6 +11,8 @@ public class MovingCurve extends PApplet {
     Point three = new Point('c', 100, 400);
     Point four = new Point('d', 100, 600);
     ArrayList<Point> stop = new ArrayList<Point>();
+    
+    ArrayList<Point> points = new ArrayList<Point>();
 
     float t = .5f;
     private static final int GRAY = 88;
@@ -19,7 +21,7 @@ public class MovingCurve extends PApplet {
     Neville currentNeville;
     Neville end;
 
-    Neville[] curves = new Neville[4];
+    Neville[] curves;
 
     public static void main(String[] args) {
         PApplet.main(new String[] { "--present", "MovingCurve" });
@@ -28,35 +30,55 @@ public class MovingCurve extends PApplet {
     public void setup() {
         size(800, 800);
         frameRate(30);
-        start.add(one);
-        start.add(two);
-        start.add(three);
-        start.add(four);
-
-        stop.add(new Point('1', 100, 300));
-        stop.add(new Point('2', 200, 350));
-        stop.add(new Point('3', 700, 200));
-        stop.add(new Point('4', 500, 600));
+        
+        
+        int numCurves = 4;
+        int ctrlPoints = 4;
+        
+        curves = new Neville[numCurves];
+        for (int i = 0; i < numCurves; i++	) {
+        	Point[] pts = new Point[ctrlPoints];
+        	for (int j = 0; j < ctrlPoints; j++) {
+        		Point p = pts[j] = new Point('x', 30 + 100*i, 30 + 100*j);
+        		points.add(p);
+        	}
+        	curves[i] = new Neville(pts);
+        }
+       
+//        points.add(one);
+//        points.add(two);
+//        points.add(three);
+//        points.add(four);
+//
+//        stop.add(new Point('1', 100, 300));
+//        stop.add(new Point('2', 200, 350));
+//        stop.add(new Point('3', 700, 200));
+//        stop.add(new Point('4', 500, 600));
+        
         smooth();
         
-        begin = new Neville(start.toArray(new Point[] {}));
+        begin = new Neville(new Point[] {one, two, three, four});
         end = new Neville(stop.toArray(new Point[] {}));
     }
 
     public void draw() {
         background(GRAY);
 
-        for(int i=0; i < start.size(); i++){
-            start.get(i).display(this);
+        for (Point p: points) {
+        	p.display(this);
+        }
+        
+        for (Neville n: curves) {
+        	n.draw(this);
         }
 
-        begin.draw_trace(this);
-        end.draw_trace(this);
-
-        PVector A = MathMagic.neville(t, begin.getCtrl_pnts());
-        PVector B = MathMagic.neville(t, end.getCtrl_pnts());
-        PVector live = MathMagic.neville(t, A, B);
-        ellipse(live.x, live.y, 30, 30);
+//        begin.draw_trace(this);
+//        end.draw_trace(this);
+//
+//        PVector A = MathMagic.neville(t, begin.getCtrl_pnts());
+//        PVector B = MathMagic.neville(t, end.getCtrl_pnts());
+//        PVector live = MathMagic.neville(t, A, B);
+//        ellipse(live.x, live.y, 30, 30);
     }
 
     private Point grabbedPoint;
@@ -64,7 +86,7 @@ public class MovingCurve extends PApplet {
     public void mouseDragged() {
         if (grabbedPoint == null) {
             float maxDistance = 400000;
-            for (Point p: start) {
+            for (Point p: points) {
                 float distance = p.distance(mouseX, mouseY);
                 if (distance < maxDistance) {
                     maxDistance = distance;
