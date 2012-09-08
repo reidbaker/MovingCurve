4e8b3c6c6b737b415b4848175a8142fbf144c4b9
Reid Baker <hamiltonbaker@yahoo.com>
2012-09-08 10:49:13 -0400

    draw a group of points

--- src/MovingCurve.java
+++ src/MovingCurve.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class MovingCurve extends PApplet {
 
+    ArrayList<Point> points = new ArrayList<Point>();
+    Point one = new Point(this, 200, 200);
+    Point two = new Point(this, 200, 300);
+    Point three = new Point(this, 200, 400);
+    Point four = new Point(this, 200, 500);
+
+    private static final int GRAY = 88;
 
     public static void main(String[] args) {
         PApplet.main(new String[] { "--present", "MovingCurve" });
@@ -9,13 +18,17 @@ public class MovingCurve extends PApplet {
 
     public void setup() {
         size(800, 600);
+        points.add(one);
+        points.add(two);
+        points.add(three);
+        points.add(four);
     }
 
     public void draw() {
-        background(100);
-        Point one = new Point(this, 200, 200);
-        one.display();
-        ellipse(mouseX, mouseY, 80, 80);
+        background(GRAY);
+        for(int i=0; i < points.size(); i++){
+            points.get(i).display();
+        }
     }
 
 }
