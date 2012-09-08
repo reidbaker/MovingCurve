fcea2e03dd042672f00f27a0c470b53a5c94db17
Reid Baker <hamiltonbaker@yahoo.com>
2012-09-08 10:58:06 -0400

    todos added

--- src/MovingCurve.java
+++ src/MovingCurve.java
@@ -31,4 +31,8 @@ public class MovingCurve extends PApplet {
         }
     }
 
+    public void mousePressed() {
+        //TODO find closest point and move it
+    }
+
 }
--- src/Point.java
+++ src/Point.java
@@ -1,9 +1,10 @@
 import processing.core.PApplet;
 
 public class Point {
-    PApplet p;
+    private PApplet p;
     private float xLoc;
     private float yLoc;
+    private int size = 30;
 
     public Point(PApplet p, float x, float y) {
         this.p = p;
@@ -12,7 +13,19 @@ public class Point {
     }
 
     public void display(){
-        p.ellipse(getXLoc(), getYLoc(), 80, 80);
+        p.ellipse(getXLoc(), getYLoc(), size, size);
+    }
+
+    public float distance(Point B){
+        //TODO return distance from here to B
+        System.out.println("Not Yet Implimented");
+        return 0;
+    }
+
+    public float midpoint(Point B){
+        //TODO find midpoint between here and B
+        System.out.println("Not Yet Implimented");
+        return 0;
     }
 
     public float getYLoc() {
@@ -30,6 +43,14 @@ public class Point {
     public void setXY(float x, float y){
         this.xLoc = x;
         this.yLoc = y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
     }
 
 
